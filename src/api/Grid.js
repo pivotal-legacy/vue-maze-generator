ea6d7dd946bb3e64e2c9c875e48606530bb17e04
Robert Gravina <rgravina@pivotal.io>
2017-09-04 14:25:49 +0900

    Set cell neibours.

--- src/api/Grid.js
+++ src/api/Grid.js
@@ -4,7 +4,9 @@ const Grid = class {
   constructor({rows, columns}) {
     this.rows = rows
     this.columns = columns
+    this.size = rows * columns
     this.grid = this.prepareGrid()
+    this.configureCells()
   }
 
   toString() {
@@ -17,6 +19,40 @@ const Grid = class {
         return new Cell({row, column})
       })
     })
+  }
+
+  configureCells() {
+    this.eachCell((cell) => {
+      const {row, column} = cell
+      cell.north = this.getGrid(row - 1, column)
+      cell.south = this.getGrid(row + 1, column)
+      cell.west = this.getGrid(row, column - 1)
+      cell.east = this.getGrid(row, column + 1)
+    })
+  }
+
+  eachCell(fn) {
+    this.eachRow((row) => {
+      for (let cell of row) {
+        fn(cell)
+      }
+    })
+  }
+
+  eachRow(fn) {
+    for (let row of this.grid) {
+      fn(row)
+    }
+  }
+
+  getGrid(row, column) {
+    if (row < 0 || row >= this.rows) {
+      return undefined
+    }
+    if (column < 0 || column >= this.columns) {
+      return undefined
+    }
+    return this.grid[row][column]
   }
 }
 
--- test/api/Grid.test.js
+++ test/api/Grid.test.js
@@ -2,10 +2,11 @@ import test from 'ava';
 import Grid from '../../src/api/Grid'
 import Cell from '../../src/api/Cell'
 
-test('it should have a position', t => {
+test('it should have a position and cells created at these positions', t => {
   const grid = new Grid({rows: 2, columns: 3})
   t.is(grid.rows, 2);
   t.is(grid.columns, 3);
+  t.is(grid.size, 6);
   t.is(grid.grid.length, 2)
   t.is(grid.grid[0].length, 3)
   t.is(grid.grid[0][0].row, 0)
@@ -14,4 +15,13 @@ test('it should have a position', t => {
   t.is(grid.grid[1][0].column, 0)
   t.is(grid.grid[0][1].row, 0)
   t.is(grid.grid[0][1].column, 1)
+});
+
+
+test('cells should have neighbors defined', t => {
+  const grid = new Grid({rows: 3, columns: 3})
+  t.is(grid.grid[1][1].north, grid.grid[0][1])
+  t.is(grid.grid[1][1].south, grid.grid[2][1])
+  t.is(grid.grid[1][1].west, grid.grid[1][0])
+  t.is(grid.grid[1][1].east, grid.grid[1][2])
 });
